b3343a3578b15a8d26c362f83a68c9eec5c382d1
Muhsin Deniz <muhsin.deniz@arneca.com>
2021-02-16 21:49:53 +0300

    Javascript temelleri ve css

---
+++ Javascript - Ders -6/main.js
@@ -0,0 +1,17 @@
+var users = document.querySelectorAll(".umut");
+
+for (let i = 0; i < users.length; i++) {
+    console.log(users[i].innerText)
+}
+
+let a = 5;
+const b = 5;
+var x = 4;
+
+function hesapla() {
+var num1 = document.getElementById("num1").value;
+var num2 = document.getElementById("num2").value;
+
+console.log(num1+num2)
+
+}
